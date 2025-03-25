870bd686575e0bf43ac2e13f5f44b7bcf4e288a1
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-25 10:20:10 +0100

    scaffold + alap

---
+++ Models/Scholarship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OsztondijasDiakProjekt.Models;
+
+public partial class Scholarship
+{
+    public string? Email { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? ScholarshipAmount { get; set; }
+}
