cc82218f906b5d5ed583ee201a55742ba9bf65ce
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-25 10:26:50 +0100

    1. feladat

---
+++ Models/ScholarshipApplicant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsztondijasDiakProjekt.Models
+{
+    public class ScholarshipApplicant
+    {
+        private double _scholarshipAmount;
+        private string _email;
+
+        public ScholarshipApplicant(int scholarshipAmount, string email, string name)
+        {
+            _scholarshipAmount = scholarshipAmount;
+            _email = email;
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public string Email { get => _email; set => _email = value; }
+        public double ScholarshipAmount { get => _scholarshipAmount; set => _scholarshipAmount = value; }
+    
+        public void IncreaseAmount(double increase)
+        {
+            _scholarshipAmount += increase;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Email}) => {ScholarshipAmount}Ft";
+        }
+    }
+}
