18091f2e17ee7d3640de79414d17c513ffa075b2
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-03-25 10:38:38 +0100

    4. feladat

---
+++ Repos/ScholarshipRepo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OsztondijasDiakProjekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsztondijasDiakProjekt.Repos
+{
+    public class ScholarshipRepo
+    {
+        private readonly ScholarshipContext _context;
+
+        public ScholarshipRepo(ScholarshipContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> GetNumberOfScholarshipApplicants()
+        {
+            return _context.Scholarships.CountAsync();
+        }
+    }
+}
